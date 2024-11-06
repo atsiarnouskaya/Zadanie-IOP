e6c02296c33674641691c514c285d48697b52194
atsiarnouskaya <atsiarnouskaya@mat.umk.pl>
2024-11-06 22:49:10 +0100

    Dodana f-cja fibbonaci w program.cpp

--- program.cpp
+++ program.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int fibonacci(int k){
+	if (k == 0) return 0;
+	if (k == 1) return 1;
+	return fibonacci(k - 1) + fibonacci(k - 2);
+}
 int main(){
 
 	int n;
 	cin >> n;
-
-	cout << n << endl;
+	cout << n <<"-ta liczba Fibbonaciego " << fibonacci(n) << endl;
 	return 0;
 }
